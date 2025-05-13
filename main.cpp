73710bf01ca82aa3ca8b6b0ac695da74eec68df9
Fernando Rocha <A00836634@tec.mx>
2025-05-12 20:47:28 -0600

    Multiplicaciones

--- main.cpp
+++ main.cpp
@@ -3,13 +3,16 @@
 #include "sum.h"
 #include "sub.h"
 #include "div.h"
+#include "multi.h"
 
 using std::cout;
 
-int main() {
+int main()
+{
 
     std::cout << "sum(1, 5) = " << sum(1, 5) << '\n';
     std::cout << "sub(5, 1) = " << sub(5, 1) << '\n';
     std::cout << "div(9, 3) = " << divide(9, 3) << '\n';
+    std::cout << "multi(5, 5) = " << multiply(5, 5) << '\n';
     return 0;
 }
---
+++ test/test_multiply.cpp
@@ -0,0 +1,16 @@
+#include <gtest/gtest.h>
+#include "multi.h"
+
+TEST(MultiTest, HandlesPositiveInput)
+{
+    int multi = multiply(5, 5);
+
+    EXPECT_EQ(multi, 25);
+}
+
+TEST(MultiTest, HandlesZeroInput)
+{
+    int multi = multiply(5, 0);
+
+    EXPECT_EQ(result, 0);
+}
